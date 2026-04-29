fb887f05c9513be736b5130000c0195287d16d54
syzyf21 <hubert.nowacki21@pm.me>
2026-04-29 20:48:00 +0200

    added folder check and improved cleanliness

--- log-archive.sh
+++ log-archive.sh
@@ -3,19 +3,30 @@
 log_source=$1
 time_to_log=$2
 time_to_delete=$3
+log_archive_path=~/log-archive
+archives_path=~/log-archive/archives
 
-mkdir ~/log-archive
+#checks if log-archive directory exists
+if [ ! -d $log_archive_path ]; then
+  mkdir $log_archive_path
+fi
 
+#checks if archives directory exists
+if [ ! -d $archives_path ]; then
+   mkdir $archives_path
+fi
+
+#
 if [ -z "$1" ]; then
-	echo "Provide a path to a directory from which i should archive your logs"
+  echo "Provide a path to a directory from which i should archive your logs"
 else
-	if [ -z "$2" ]; then
-		cp $1/*.log ~/log-archive/archives/
-	else
-		while true; do
-			cp $1/*.log ~/log-archive/archives/
-			sleep $2s
-		done
-	fi
+  if [ -z "$2" ]; then
+    cp $1/*.log $archives_path
+  else
+    while true; do
+      cp $1/*.log $archives_path
+      sleep $2s
+    done
+  fi
 fi
 
